f7f03bbf3f40aa2fc419666eff4dd7a33aba2cc9
glendc <glen.decauwsemaecker@otainsight.com>
2022-05-27 21:05:03 +0200

    fix deps & scripts

--- scripts/code-analysis.sh
+++ scripts/code-analysis.sh
@@ -2,6 +2,6 @@
 
 cargo fmt --all -- --check && \
     cargo clippy --all-targets -- -D warnings && \
-    cargo check --bins && \
+    cargo check --all && \
     cargo doc --no-deps --all && \
     cargo test --all
